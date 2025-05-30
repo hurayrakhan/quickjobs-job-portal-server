1b17d2657fc604fbfade459ac0620fb4b93135f7
Abu Hurayra Khan <imhurayrakhan@gmail.com>
2025-05-30 22:58:32 +0600

    added delete function api

--- index.js
+++ index.js
@@ -42,6 +42,14 @@ async function run() {
         res.send(result)
     })
 
+    // delete data
+    app.delete('/jobs/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id : new ObjectId(id)};
+      const result = await jobCollection.deleteOne(query);
+      res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
