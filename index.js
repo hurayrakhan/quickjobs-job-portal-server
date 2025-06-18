b56d22f725891de721730a5b71fbbe37d1147ce3
Abu Hurayra Khan <imhurayrakhan@gmail.com>
2025-06-18 23:53:32 +0600

    mverify token

--- index.js
+++ index.js
@@ -20,6 +20,23 @@ const client = new MongoClient(uri, {
   }
 });
 
+// verify firebase token
+const verifyFirebaseToken = async (req, res, next) => {
+  const authHeader = req.headers?.authorization;
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    return res.status(401).send({ message: 'unauthorized access' })
+  }
+  const token = authHeader.split(' ')[1];
+  try {
+    const decoded = await admin.auth().verifyIdToken(token);
+    req.decoded = decoded
+    next()
+  }
+  catch (error) {
+    return res.status(401).send({ message: 'unauthorized access' })
+  }
+};
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -95,6 +112,9 @@ useEffect(() => {
         });
         return () => unsubscribe();
     }, []);
+
+
+    for fetch use {credentials: 'include'}
 
 
 Then receive data from server side
